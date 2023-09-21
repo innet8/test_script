a2a46a598472cd2ae243a0e61279c35b800e117a
huangchengwu <751164212@qq.com>
2023-09-21 11:06:49 +0800

    add

--- auto_ioread/test_ioread.sh
+++ auto_ioread/test_ioread.sh
@@ -17,8 +17,7 @@ for i in $(ls result); do
 
     _WRITE=$(printf "%.0f" $(echo ${WRITE} | awk '{print $2}' | sed 's#(##g;s#)##g;s#MB/s##g'))
 
-    echo $_WRITE
-
+ 
     shell_write_data=$shell_write_data"$_WRITE,"
     shell_read_data=$shell_read_data"$_READ,"
     shell_ip=$shell_ip"\"$ip\","
@@ -29,9 +28,6 @@ done
 shell_ip=$(echo $shell_ip | sed 's#,$##g')"]"
 shell_write_data=$(echo $shell_write_data | sed 's#,$##g')"]"
 shell_read_data=$(echo $shell_read_data | sed 's#,$##g')"]"
-echo $shell_ip
-echo $shell_write_data
-echo $shell_read_data
 
 eval "cat  <<EOF
         $(<./template.html)
